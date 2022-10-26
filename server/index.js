65287ec1d13b4c24091e7054497d6f3a0b0057c9
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-10-26 17:56:32 +0200

    Added dotenv config

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import * as dotenv from 'dotenv'
 import authRouter from './routes/auth/auth.js';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
