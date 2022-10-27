20af51d142352d826628432f166678911af6a1c5
Kravchuk Andrey <aksnt13@gmail.com>
2022-10-27 16:34:41 +0200

    continue fight

--- client/src/pages/Fight.js
+++ client/src/pages/Fight.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import fighter from '../assets/images/fighter.png'
+import fighter2 from '../assets/images/reversefighter.png'
 import { FighterStyled } from './Fighter.styled';
+import '../App.css'
 
 export const Fight = ()=> {
     return(
         <>
-            <FighterStyled src={fighter} alt="" />
-            <div>
-                <div>
+            
+            <div className='flex'>
+                <div className="flex2">
+                    <FighterStyled src={fighter} alt="" />
                     <h2>Attack</h2>
+                    <div className='checkbox'>
+                        <label>
+                            <input type="checkbox" name="Head"/>
+                            Head
+                        </label>
+                        <label>
+                            <input type="checkbox" name="Body"/>
+                            Body
+                        </label>
+                        <label>
+                            <input type="checkbox" name="Legs"/>
+                            Legs
+                        </label>
+                    </div>
                 </div>
-                <div>
+                <div className="flex2">                    
                     <h2>Defence</h2>
+                    <label>
+                        <input type="checkbox" name="Head"/>
+                        Head
+                    </label>
+                    <label>
+                        <input type="checkbox" name="Body"/>
+                        Body
+                    </label>
+                    <label>
+                        <input type="checkbox" name="Legs"/>
+                        Legs
+                    </label>
+                    <FighterStyled src={fighter2} alt="" />
                 </div>
             </div>
         </>
