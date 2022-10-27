964ec279f5cdd025ad8feeac10199f863da72303
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2022-10-27 14:01:55 +0200

    Created services folder. Split hash and verify password to different services

---
+++ server/services/hashPassword.js
@@ -0,0 +1,22 @@
+import argon2 from "argon2";
+
+const hashingOptions = {
+    type: argon2.argon2id,
+    memoryCost: 2 ** 16,
+    timeCost: 5,
+    parallelism: 1,
+}
+
+export const hashPassword = (req, res, next) => {    
+    argon2
+        .hash(req.body.password, hashingOptions)
+        .then((password_hash)=>{            
+            req.body.password_hash = password_hash;
+            delete req.body.password;            
+            next();
+        })
+        .catch((err)=> {
+            console.error(err);
+            res.sendStatus(500);
+        });
+};
---
+++ server/services/verifyPassword.js
@@ -0,0 +1,17 @@
+import argon2 from "argon2";
+
+export const verifyPassword = (req, res) => {
+    argon2
+        .verify(req.user.password_hash, req.body.password)
+        .then((isVerified) => {
+            if (isVerified) {
+                res.send("password right");
+            } else {
+                res.send('password error');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.sendStatus(500);
+        });
+};
