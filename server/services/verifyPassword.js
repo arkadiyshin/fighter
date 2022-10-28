fed11d450b7c6a3a26227555cf5a8968543666da
oleksandrsokaliuk <108679816+oleksandrsokaliuk@users.noreply.github.com>
2022-10-28 14:34:08 +0200

    changed login button

--- server/services/verifyPassword.js
+++ server/services/verifyPassword.js
@@ -1,20 +1,19 @@
 import argon2 from "argon2";
 
-
 const verifyPassword = (req, res) => {
-    argon2
-        .verify(req.user.password_hash, req.body.password)
-        .then((isVerified) => {
-            if (isVerified) {
-                res.send("password right");
-            } else {
-                res.send('password error');
-            }
-        })
-        .catch((err) => {
-            console.error(err);
-            res.sendStatus(500);
-        });
+  argon2
+    .verify(req.user.password_hash, req.body.password)
+    .then((isVerified) => {
+      if (isVerified) {
+        res.send(req.user);
+      } else {
+        res.send("password error");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 export default verifyPassword;
