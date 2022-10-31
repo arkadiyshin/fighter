97c59f2ef0ba13e15bd5c83ba567a366cd1f53fb
oleksandrsokaliuk <108679816+oleksandrsokaliuk@users.noreply.github.com>
2022-10-31 16:22:38 +0100

    changed background

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -3,6 +3,7 @@ import { Logo } from "../components/Logo";
 import { GameContainer, MainDiv } from "./Main.styled";
 import { Map } from "../components/Map";
 import { UserInfo } from "../components/UserInfo";
+import axios from "axios";
 
 export const Main = () => {
   return (
